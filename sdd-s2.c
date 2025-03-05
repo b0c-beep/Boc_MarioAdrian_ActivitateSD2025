10b35c599b58c9149832289e25c7b573b43ffebf
b0c-beep <bocmarioadrian98@gmail.com>
2025-03-05 16:32:57 +0200

    modificat sablon 2 initial

--- sdd-s2.c
+++ sdd-s2.c
@@ -1,49 +1,58 @@
 #include<stdio.h>
 #include<malloc.h>
 
-struct Sablon {
+struct Masina {
 	int id;
-	//int ;
-	//char* ;
-	//float ;
-	//char ;
+	int anFabricatie;
+	char* sofer;
+	float kilometriiParcursi;
+	char initialaProducator;
 };
-struct Sablon initializare(/*parametri necesari*/) {
-	struct Sablon s;
-	s.id = 1;
-	//initializare structura 
-	return s;
+
+struct Masina initializare(int id, int anFabricatie, const char* sofer, float kilometriiParcursi, char initialaProducator) {
+	struct Masina m;
+	m.id = id;
+	m.anFabricatie = anFabricatie;
+	m.sofer = (char*)malloc(strlen(sofer) + 1);
+	strcpy_s(m.sofer, strlen(sofer) + 1, sofer);
+	m.kilometriiParcursi = kilometriiParcursi;
+	m.initialaProducator = initialaProducator;
+	return m;
 }
 
-void afisare(struct Sablon s) {
-	//afisarea tuturor atributelor.
+void afisare(struct Masina m) {
+	printf("ID: %d \n", m.id);
+	printf("An fabricatie: %d \n", m.anFabricatie);
+	printf("Sofer: %s \n", m.sofer);
+	printf("Kilometrii Parcursi : %f \n", m.kilometriiParcursi);
+	printf("Initiala producator: %c \n", m.initialaProducator);
 }
 
-void afisareVector(struct Sablon* vector, int nrElemente) {
+void afisareVector(struct Masina* vector, int nrElemente) {
 	//afisarea elementelor din vector apeland functia afisare
 }
 
-struct Sablon* copiazaPrimeleNElemente(struct Sablon* vector, int nrElemente, int nrElementeCopiate) {
+struct Masina* copiazaPrimeleNElemente(struct Masina* vector, int nrElemente, int nrElementeCopiate) {
 	//copiem intr-un vector nou pe care il vom returna primele nrElementeCopiate
-	struct Sablon *vectorNou=NULL;
+	struct Masina *vectorNou=NULL;
 
 	return vectorNou;
 }
 
-void dezalocare(struct Sablon** vector, int* nrElemente) {
+void dezalocare(struct Masina** vector, int* nrElemente) {
 	//dezalocam elementele din vector si vectorul
 }
 
-void copiazaAnumiteElemente(struct Sablon* vector, char nrElemente, float prag, struct Sablon** vectorNou, int* dimensiune) {
+void copiazaAnumiteElemente(struct Masina* vector, char nrElemente, float prag, struct Masina** vectorNou, int* dimensiune) {
 	//parametrul prag poate fi modificat in functie de 
 	// tipul atributului ales pentru a indeplini o conditie
 	//este creat un nou vector cu elementele care indeplinesc acea conditie
 }
 
-struct Sablon getPrimulElementConditionat(struct Sablon* vector, int nrElemente, const char* conditie) {
+struct Masina getPrimulElementConditionat(struct Masina* vector, int nrElemente, const char* conditie) {
 	//trebuie cautat elementul care indeplineste o conditie
 	//dupa atributul de tip char*. Acesta este returnat.
-	struct Sablon s;
+	struct Masina s;
 	s.id = 1;
 
 	return s;
