d8d8a99b2d9bbc91020df0271e54bb12fa4fed1d
b0c-beep <bocmarioadrian98@gmail.com>
2025-03-12 17:06:41 +0200

    terminat primele 3 functii

--- sdd-s3.c
+++ sdd-s3.c
@@ -3,9 +3,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-//trebuie sa folositi fisierul masini.txt
-//sau va creati un alt fisier cu alte date
-
 struct StructuraMasina {
 	int id;
 	int nrUsi;
@@ -17,22 +14,34 @@ struct StructuraMasina {
 typedef struct StructuraMasina Masina;
 
 void afisareMasina(Masina masina) {
-	//afiseaza toate atributele unei masini
+	printf("ID = %d\n", masina.id);
+	printf("Numar usi = %d\n", masina.nrUsi);
+	printf("Pret = %.2f\n", masina.pret);
+	printf("Model = %s\n", masina.model);
+	printf("Nume Sofer = %s\n", masina.numeSofer);
+	printf("Serie = %c\n", masina.serie);
 }
 
 void afisareVectorMasini(Masina* masini, int nrMasini) {
-	//afiseaza toate elemente de tip masina din vector
-	//prin apelarea functiei afisareMasina()
+	for (int i = 0; i < nrMasini; i++) {
+		afisareMasina(masini[i]);
+	}
 }
 
 void adaugaMasinaInVector(Masina** masini, int* nrMasini, Masina masinaNoua) {
-	//adauga in vectorul primit o noua masina pe care o primim ca parametru
-	//ATENTIE - se modifica numarul de masini din vector;
+	Masina* aux = (Masina*)malloc(sizeof(Masina) * ((*nrMasini) + 1));
+	for (int i = 0; i < (*nrMasini); i++)
+	{
+		aux[i] = (*masini)[i];
+	}
+	aux[(*nrMasini)] = masinaNoua;
+	free(*masini);
+	(*masini) = aux;
+	(*nrMasini) += 1;
 }
 
 Masina citireMasinaFisier(FILE* file) {
-	//functia citeste o masina dintr-un strceam deja deschis
-	//masina citita este returnata;
+	
 }
 
 Masina* citireVectorMasiniFisier(const char* numeFisier, int* nrMasiniCitite) {
@@ -47,7 +56,7 @@ void dezalocareVectorMasini(Masina** vector, int* nrMasini) {
 }
 
 int main() {
-
+	
 
 	return 0;
 }
